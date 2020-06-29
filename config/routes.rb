e9bd293d9646f6e4af5793440ec896bc1cddcb1d
Sara Vieira <s.cvieira@sapo.pt>
2020-06-29 18:31:41 +0100

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :teachers do
+    resources :teacher_subjects, only: [:create, :destroy]
+    resources :groups
+    resources :flashcards_set
+    resources :flashcards do
+      resources :student_flashcards
+    end
+    resources :homework
+  end
+  resources :students do
+    resources :student_flashcards_set
+    resources :student_flashcards
+  end
+  resources :topics
 end
