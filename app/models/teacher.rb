1b984bad6c3f431b7fcf19c9b92db0b66b40dd63
Sara Vieira <s.cvieira@sapo.pt>
2020-07-01 14:49:48 +0100

    made teacher_subjects table, along with seeds for topics, subjects and teacher_subjects

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,5 @@
 class Teacher < ApplicationRecord
   belongs_to :user
+  has_many :teacher_subjects
   validates :first_name, :last_name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,15 @@ require "open-uri"
 puts 'Cleaning the database...'
 User.destroy_all
 
+puts 'Creating subjects and topics...'
+subjects = ['Maths', 'Biology', 'Geology', 'Physics', 'Chemistry', 'English', 'French', 'German', 'Phylosophy', 'History', 'Geography', 'Economics', 'Sociology', 'Psychology']
+subjects.each do |subject|
+  new_subject = Subject.create(subject_name: subject)
+  10.times do
+    new_topic = Topic.new(topic_name: Faker::Educator.subject)
+  end
+end
+
 puts 'Creating teachers...'
 10.times do
   new_user = User.new(
@@ -24,12 +33,14 @@ puts 'Creating teachers...'
   #   content_type: 'image/png'
   #   )
   new_user.save
+
   new_teacher = Teacher.new(
     user: new_user,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     )
   new_teacher.save
+  TeacherSubject.create(teacher: new_teacher, subject: Subject.find_by(subject_name: subjects.sample))
   # rescue ActiveStorage::IntegrityError
   #   file = URI.open("https://images.unsplash.com/photo-1549068106-b024baf5062d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80")
   #   new_teacher.photo.attach(
@@ -37,14 +48,15 @@ puts 'Creating teachers...'
   #     filename: "profilepic.png",
   #     content_type: 'image/png'
   #     )
-    5.times do
-      new_group = Group.new(
-        teacher: new_teacher,
-        class_name: "#{Faker::Educator.course_name}: #{rand(1..30)}"
-        )
-      new_group.save
-      end
-    end
+
+  5.times do
+    new_group = Group.new(
+      teacher: new_teacher,
+      class_name: "#{Faker::Educator.course_name}: #{rand(1..30)}"
+      )
+    new_group.save
+  end
+end
 
 puts 'Creating Students...'
 100.times do
@@ -60,12 +72,14 @@ puts 'Creating Students...'
   #   content_type: 'image/png'
   #   )
   new_user.save
+
   new_student = Student.new(
     user: new_user,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     )
   new_student.save
+
   # rescue ActiveStorage::IntegrityError
   #   file = URI.open("https://images.unsplash.com/photo-1549068106-b024baf5062d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80")
   #   new_teacher.photo.attach(
@@ -74,4 +88,5 @@ puts 'Creating Students...'
   #     content_type: 'image/png'
   #     )
 end
+
 puts 'Done!'
