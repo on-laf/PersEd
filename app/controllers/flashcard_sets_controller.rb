24603521226539396654ac19732722dca44d79e3
Eoin Lafferty <eoinlafferty@gmail.com>
2020-07-01 20:01:27 +0100

    added flashcard_set MVC. Could do with some bootstrap to finish

--- app/controllers/flashcard_sets_controller.rb
+++ app/controllers/flashcard_sets_controller.rb
@@ -1,16 +1,19 @@
 class FlashcardSetsController < ApplicationController
+  before_action :find_flashcard_set, only: [:show, :edit, :update, :destroy]
 
   def index
-    @flashcard_sets = FlashcardSet.all
+    @flashcard_sets = policy_scope(FlashcardSet)
   end
 
   def new
     @flashcard_set = FlashcardSet.new
+    authorize @flashcard_set
   end
 
   def create
     @flashcard_set = FlashcardSet.new(flashcard_set_params)
     @flashcard_set.teacher = current_teacher
+    authorize @flashcard_set
     if @flashcard_set.save
       redirect_to @flashcard_set
     else
@@ -19,15 +22,15 @@ class FlashcardSetsController < ApplicationController
   end
 
   def show
-    @flashcard_set = FlashcardSet.find(params[:id])
+    authorize @flashcard_set
   end
 
   def edit
-    @flashcard_set = FlashcardSet.find(params[:id])
+    authorize @flashcard_set
   end
 
   def update
-    @flashcard_set = FlashcardSet.find(params[:id])
+    authorize @flashcard_set
     if @flashcard_set.update(flashcard_set_params)
       redirect_to(@flashcard_set)
     else
@@ -36,10 +39,14 @@ class FlashcardSetsController < ApplicationController
   end
 
   def destroy
-    @flashcard_set = FlashcardSet.find(params[:id])
+    authorize @flashcard_set
   end
 
   private
+
+  def find_flashcard_set
+    @flashcard_set = FlashcardSet.find(params[:id])
+  end
 
   def flashcard_set_params
     params.require(:flashcard_set).permit(:name)
