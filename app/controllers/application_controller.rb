7cb6595ed8320019186ec8f4327d23cb3b2ef860
michal szymkat <mikeszymkat@gmail.com>
2020-07-03 16:42:20 +0100

    cleaning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:teacher?])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:teacher?])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
   end
 
   include Pundit
