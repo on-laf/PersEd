ed78864d4310d1fa72bceabe4b8478c825d4df58
Sara Vieira <s.cvieira@sapo.pt>
2020-07-03 11:05:21 +0100

    move on with group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :find_group, only: [:show, :edit, :update, :destroy]
+
   def new
     @group = Group.new
   end
@@ -14,7 +16,21 @@ class GroupsController < ApplicationController
     end
   end
 
+  def index
+    @groups = policy_scope(Group)
+  end
+
+  def show; end
+
+  def edit; end
+
+
   private
+
+  def find_group
+    @group = Group.find(params[:id])
+    authorize @group
+  end
 
   def group_params
     params.require(:group).permit(:class_name, :teacher_id)
