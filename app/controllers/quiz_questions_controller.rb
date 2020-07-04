216db5a70606cc7d90c24d7507b47e891559ad31
Eoin Lafferty <eoinlafferty@gmail.com>
2020-07-04 12:20:59 +0100

    controller for quiz questions

---
+++ app/controllers/quiz_questions_controller.rb
@@ -0,0 +1,24 @@
+class QuizQuestionsController < ApplicationController
+  before_action :find_quiz_question, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
