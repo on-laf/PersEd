13ae3c23f3d4a43659701ce977dc7f44742e50ae
Eoin Lafferty <eoinlafferty@gmail.com>
2020-07-06 09:19:24 +0100

    added pundit and controller for quizes

---
+++ app/controllers/quiz_controller.rb
@@ -0,0 +1,34 @@
+class QuizController < ApplicationController
+  before_action :find_quiz, only: [:show, :edit, :update, :destroy]
+
+def index
+end
+
+def show
+end
+
+def new
+end
+
+def create
+
+end
+
+def update
+
+end
+
+def destroy
+
+end
+
+  private
+
+  def quiz
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def quiz_params
+    params.require(:quiz).permit(:quiz_name, :teacher_id)
+  end
+end
